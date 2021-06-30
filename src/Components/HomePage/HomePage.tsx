ebe9d18a7299df6ff309041265c84af9fdf45df9
Eric Olaveson <olavesoneric@gmail.com>
2021-06-30 09:40:18 -0500

    stub

---
+++ src/Components/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+//Library imports
+import React from 'react'
+import { Button } from '@material-ui/core';
+
+//Page imports
+
+function HomePage() {
+    return (
+        <div>
+            <Button color="primary">Take Survey</Button> 
+        </div>
+    )
+}
+
+export default HomePage
